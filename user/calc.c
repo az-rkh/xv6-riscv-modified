6f24d8742d8acdc6de8063273e2142cae08e0d65
Azamat Rakhim <azamatrakhim8@gmail.com>
2026-01-13 10:23:51 +0500

    calc() exit()

--- user/calc.c
+++ user/calc.c
@@ -2,16 +2,38 @@
 #include <kernel/stat.h>
 #include <user/user.h>
 
+int parse(char *s, int *a, char *op, int *b) {
+    *a = atoi(s);
+
+    while (*s && *s != ' ') {
+        s++;
+    }
+    while (*s == ' ') {
+        s++;
+    }
+    
+    *op = *s;
+    s++;
+
+    while (*s == ' ') {
+        s++;
+    }
+
+    *b = atoi(s);
+    s++;
+    return 0;
+}
+
 int main(void) {
     char type[99] = {0};
     int a = 0, b = 0;
     char op = 0; 
     // int res;
     printf("======= Calculator ========");
-    printf("Type |calculate| to perform mathematical calculation, or |exit| to exit this program");
+    printf("Type |calculate| to perform mathematical calculation, or |exit| to exit this program\n");
     read(0, type, sizeof(type));
 
-    parser(type, &a, &op, &b);
+    parse(type, &a, &op, &b);
 
     printf("Parsed: %d, %c, %d", a, op, b);
 
@@ -19,4 +41,8 @@ int main(void) {
     //     printf("Type something\n");
     //     exit(0);
     // }
+    if (strcmp(type, "exit")) {
+        printf("Exiting program.\n");
+        exit(0);
+    }
 }

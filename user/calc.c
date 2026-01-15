23981f8bea58765d78033a847cb5502aec24012e
Azamat Rakhim <azamatrakhim8@gmail.com>
2026-01-15 10:39:36 +0500

    calc() main()

--- user/calc.c
+++ user/calc.c
@@ -11,7 +11,7 @@ int parse(char *s, int *a, char *op, int *b) {
     while (*s >= '0' && *s <= '9' && *s != '\0') {
         s++;
     }
-    
+
     while (*s == ' ' && *s != '\0') {
         s++;
     }
@@ -36,21 +36,38 @@ int main(void) {
     char type[99] = {0};
     int a = 0, b = 0;
     char op = 0; 
-    // int res;
+    int res = 0;
     printf("======= Calculator ========\n");
-    printf("Type |calculate| to perform mathematical calculation, or |exit| to exit this program\n");
+    printf("Type |calculate| to perform mathematical calculation,\n or |exit| to exit this program\n");
     read(0, type, sizeof(type));
-
-
-    // if (type[0] == ' ') {
-    //     printf("Type something\n");
-    //     exit(0);
-    // }
-    if (!strcmp(type, "exit")) {
+    
+    if (!strcmp(type, 'exit')) {
         printf("Exiting program.\n");
         exit(0);
     }
 
     parse(type, &a, &op, &b);
-    printf("Parsed: %d, %c, %d\n", a, op, b);
+
+    int abort = 0;
+
+    while (abort == 0) {
+        if (op == '+') {
+            res = a + b;
+        }
+        else if (op == '-') {
+            res = a - b;
+        }
+        else if (op == '*') {
+            res = a * b;
+        }
+        else if (op == '/') {
+            if (b == 0) {
+                printf("cannot divide by 0");
+                exit(0);
+            }
+            res = a / b;
+        }
+    }
+
+    printf("Result: %d\n", res);
 }
